4bb6b7bccab59e60697225dceb043e06852aa294
Nagesh Kulkarni <53990271+nagesh1198@users.noreply.github.com>
2020-11-03 13:57:30 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,28 @@
+class EvenOdd{
+
+	public static void main(String args[]){
+		
+		int Array[] = {0,1,2,3,4,77,5,56,7,64};
+		
+		for(int i=0;i<10;i++){
+			if(i%2==0){
+				
+				System.out.println(Array[i]+" is even number");
+				
+				
+			}
+			else{
+				
+				System.out.println(Array[i]+" is odd number");
+				
+			}
+			
+			
+		}
+		
+		
+		
+	}
+
+
+}
